3998ff06a8aff945cd455baf6ca165816c5cccee
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-28 13:24:44 -0400

    initial commit

---
+++ kickball.rb
@@ -0,0 +1,24 @@
+#ask for team 1's name and scores
+puts "What was team 1's name?"
+print "> "
+team_one = gets.chomp
+puts "What was team 1's score?"
+print "> "
+team_one_score = gets.chomp
+puts "What was team 2's name?"
+print "> "
+team_two = gets.chomp
+puts "What was team 2's score?"
+print "> "
+team_two_score = gets.chomp
+
+puts team_one
+puts team_one_score
+puts team_two
+puts team_two_score
+
+#store this information
+#ask for team 2's name and scores
+#store this information
+#report back who the victor is
+
