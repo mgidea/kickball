501f84535f54a65a2243e67f6f89e40a02e24c51
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-28 13:58:56 -0400

    moved on to phase 3, refactor with validations later

--- kickball.rb
+++ kickball.rb
@@ -1,3 +1,23 @@
+#phase 1 pseudo code
+#ask for team 1's name and scores
+#store this information
+#ask for team 2's name and scores
+#store this information
+#compare scores
+#report back who the victor is
+victors = []
+game_scores = []
+def identifiy_winner(first_team, first_team_score, second_team, second_team_score, winner, scores)
+  if first_team_score > second_team_score
+   winner << first_team
+   scores << "#{first_team_score} - #{second_team_score}"
+    "#{first_team} is the victor!"
+  else
+    winner << second_team
+    scores << "#{second_team_score} - #{first_team_score}"
+    "#{second_team} is the victor!"
+  end
+end
 
 puts "What was team 1's name?"
 print "> "
@@ -13,17 +33,12 @@ print "> "
 team_two_score = gets.chomp
 puts
 
-if team_one_score > team_two_score
-  puts "#{team_one} is the victor!"
-else
-  puts "#{team_two} is the victor!"
-end
+puts identifiy_winner(team_one, team_one_score, team_two, team_two_score, victors, game_scores)
+puts victors.inspect
+puts game_scores.inspect
 
-
-#ask for team 1's name and scores
-#store this information
-#ask for team 2's name and scores
-#store this information
-#compare scores
-#report back who the victor is
+# phase 2 pseudo code
+# create a method that compares the scores of both teams and reports back the team name that won
+#transpose if conditional from phase 1 into method changing names of arguments and using return value
+# instead of puts inside of method
 
