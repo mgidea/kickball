3d960e00edfd7d6c6fd4ced98b1dc22b0ecb7559
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-28 13:30:04 -0400

    phase 1 functional

--- kickball.rb
+++ kickball.rb
@@ -1,4 +1,4 @@
-#ask for team 1's name and scores
+
 puts "What was team 1's name?"
 print "> "
 team_one = gets.chomp
@@ -11,14 +11,19 @@ team_two = gets.chomp
 puts "What was team 2's score?"
 print "> "
 team_two_score = gets.chomp
+puts
 
-puts team_one
-puts team_one_score
-puts team_two
-puts team_two_score
+if team_one_score > team_two_score
+  puts "#{team_one} is the victor!"
+else
+  puts "#{team_two} is the victor!"
+end
 
+
+#ask for team 1's name and scores
 #store this information
 #ask for team 2's name and scores
 #store this information
+#compare scores
 #report back who the victor is
 
